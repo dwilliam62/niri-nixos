4195a2933882e0edee8dbbea6e79106b5cf3b800
Don Williams <don.e.williams@gmail.com>
2025-07-16 04:43:34 -0400

    Moved niri waybar to its own config
    Was conflicting with hyprland waybar
    
     On branch niri-dev
     Your branch is up to date with 'origin/niri-dev'.
    
     Changes to be committed:
    	modified:   home/niri/default.nix
    	new file:   home/niri/waybar.nix

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -13,6 +13,6 @@
     ./rules.nix
     ./autostart.nix
     ./scripts.nix
-    ../../home/desktop/waybar-ddubs-2.nix
+    ./waybar.nix
   ];
 }
---
+++ home/niri/waybar.nix
@@ -0,0 +1,132 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+let
+  terminal = "kitty";
+  base00 = "0F1419";
+  base01 = "131721";
+  base03 = "3E4B59";
+  base05 = "E6E1CF";
+  base06 = "E6E1CF";
+  base07 = "F3F4F5";
+  base08 = "F07178";
+  base09 = "FF8F40";
+  base0A = "FFB454";
+  base0B = "B8CC52";
+  base0C = "95E6CB";
+  base0D = "59C2FF";
+  base0E = "D2A6FF";
+  base0F = "E6B673";
+in
+with lib; {
+  programs.waybar = {
+    enable = true;
+    package = pkgs.waybar;
+    settings = [{
+      layer = "top";
+      position = "top";
+
+      modules-left = ["sway/workspaces" "sway/window"];
+      modules-center = ["clock"];
+      modules-right = ["tray" "pulseaudio" "battery" "custom/menu"];
+
+      "sway/workspaces" = {
+        format = "{name}";
+      };
+      "sway/window" = {
+        max-length = 60;
+        separate-outputs = false;
+      };
+      "clock" = {
+        format = " {:%H:%M}";
+        tooltip-format = "<big>{:%A, %d.%B %Y }</big><tt><small>{calendar}</small></tt>";
+      };
+      "tray" = {
+        spacing = 12;
+      };
+      "pulseaudio" = {
+        format = "{icon} {volume}%";
+        format-muted = "";
+        format-icons = {
+          default = ["" "" ""];
+        };
+        on-click = "pavucontrol";
+      };
+      "battery" = {
+        states = {
+          warning = 30;
+          critical = 15;
+        };
+        format = "{icon} {capacity}%";
+        format-charging = "󰂄 {capacity}%";
+        format-plugged = "󱘖 {capacity}%";
+        format-icons = ["󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+      };
+      "custom/menu" = {
+        format = "";
+        on-click = "walker";
+      };
+    }];
+    style = ''
+      * {
+        font-size: 18px;
+        font-family: JetBrainsMono Nerd Font, Font Awesome, sans-serif;
+        font-weight: bold;
+      }
+
+      window#waybar {
+        background-color: rgba(19, 23, 33, 0.8);
+        border-radius: 15px;
+        color: #${base0F};
+        margin: 20px 10% 0 10%;
+      }
+
+      tooltip {
+        background: #${base00};
+        border: 1px solid #${base0E};
+        border-radius: 10px;
+      }
+
+      tooltip label {
+        color: #${base07};
+      }
+
+      #workspaces button {
+        padding: 0px 5px;
+        margin: 4px 3px;
+        border-radius: 10px;
+        color: #${base00};
+        background: linear-gradient(45deg, #${base0D}, #${base0E});
+        opacity: 0.5;
+        transition: all 0.3s ease-in-out;
+      }
+
+      #workspaces button.active {
+        opacity: 1.0;
+        min-width: 40px;
+      }
+
+      #workspaces button:hover {
+        opacity: 0.8;
+      }
+
+      #window,
+      #clock,
+      #tray,
+      #pulseaudio,
+      #battery,
+      #custom-menu {
+        background: transparent;
+        padding: 0 15px;
+        margin: 0 2px;
+      }
+
+      #clock { color: #${base0B}; }
+      #pulseaudio { color: #${base0D}; }
+      #battery { color: #${base08}; }
+      #custom-menu { color: #${base0E}; }
+    '';
+  };
+}
