3a36411fd2b87277a6ede8d4b1a73d28cf06dcd7
Don Williams <don.e.williams@gmail.com>
2025-07-16 06:26:26 -0400

    Trying to fix wf.nix script

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -3,16 +3,14 @@
   pkgs,
   inputs,
   ...
-}:
-
-{
+}: {
   imports = [
-    inputs.niri.homeModules.niri  # Import Niri's home-manager module
-    ./settings.nix                # Your custom configuration files for Niri
+    inputs.niri.homeModules.niri # Import Niri's home-manager module
+    ./settings.nix # Your custom configuration files for Niri
     ./keybinds.nix
     ./rules.nix
     ./autostart.nix
-    ./scripts.nix
+    ./scripts/wf.nix
     ./waybar.nix
   ];
 }
