4f6d343cca599e2b772e43cd26dc485371a8b5ff
Don Williams <don.e.williams@gmail.com>
2025-07-16 04:58:23 -0400

    Made waybar its own config and set niri autostart
     On branch niri-dev
     Your branch is up to date with 'origin/niri-dev'.
    
     Changes to be committed:
    	modified:   home/niri/autostart.nix
    	modified:   home/niri/waybar.nix

--- home/niri/autostart.nix
+++ home/niri/autostart.nix
@@ -5,8 +5,8 @@
     { command = ["systemctl" "--user" "start" "hyprpolkitagent"]; }
     { command = ["arrpc"]; }
     { command = ["xwayland-satellite"]; }
-    { command = ["qs"]; }
-    { command = ["waybar"]; }
+    # { command = ["qs"]; }
+    { command = ["waybar" "-c" "/home/dwilliams/.config/waybar-niri/config" "-s" "/home/dwilliams/.config/waybar-niri/style.css"]; }
     { command = ["waypaper" "--restore" ]; }
     { command = ["discordcanary"]; }
     { command = ["${pkgs.swaybg}/bin/swaybg" "-o" "Virtual-1" "-i" "~/Pictures/wallpapers/Anime-Purple-eyes.png" "-m" "fill"]; }
