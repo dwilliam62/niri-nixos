8c26464890873ac0f40c8abf6243191581cbd3d3
Don Williams <Don.e.willilams@gmail.com>
2025-07-16 21:47:53 -0400

    Updated flake added nh, thunar and fonts
     On branch niri-dev
     Your branch is up to date with 'origin/niri-dev'.
    
     Changes to be committed:
    	modified:   flake.lock
    	new file:   system/fonts.nix
    	new file:   system/programs/nh.nix
    	new file:   system/programs/thunar.nix

---
+++ system/programs/thunar.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  ...
+}: 
+  programs = {
+    thunar = {
+      enable = thunarEnable;
+      plugins = with pkgs.xfce; [
+        thunar-archive-plugin
+        thunar-volman
+      ];
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    ffmpegthumbnailer # Need For Video / Image Preview
+  ];
+}
