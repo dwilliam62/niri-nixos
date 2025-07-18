a70a9807800995b9db3b406333ffae106824edc2
Don Williams <Don.e.willilams@gmail.com>
2025-07-18 04:27:50 -0400

    Disabled Quickshell in HL

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -143,7 +143,7 @@
         #"waypaper --restore"
         "swaync"
         "ironbar"
-        "qs" # Run quickshell
+        #"qs" # Run quickshell
         #"waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css"
       ];
       # Startup ends
