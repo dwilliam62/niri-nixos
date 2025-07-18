de1299bb771765234ac682142ea0f92f4d9ff438
Don Williams <Don.e.willilams@gmail.com>
2025-07-18 08:43:35 -0400

    Adding dbus-activate to startup
     On branch niri-dev
     Your branch is up to date with 'origin/niri-dev'.
    
     Changes to be committed:
    	modified:   home/desktop/hyprland.nix

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -137,6 +137,10 @@
       # Startup starts
       exec-once = [
         "hyprctl setcursor Adwaita 24"
+        "wl-paste --type text --watch cliphist store" # Saves text
+        "wl-paste --type image --watch cliphist store" # Saves images
+        "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
+        "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user start hyprpolkitagent"
         "[workspace 1] ghostty"
         #"[workspace 3] discord"
